b788bdb40c73ec29f519c2dfc3bd90d0451c77f2
leoalmPT <leonardoalmeida7777@gmail.com>
2022-06-07 19:17:48 +0100

    .

--- src/Functions2.py
+++ src/Functions2.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 from PIL import Image
 import imagehash
 import os
@@ -174,29 +175,35 @@ class Functions:
         """
         if self.general_map.get(update[5]) is None:
             print("Update with new image: ", update[5])
-            if update[0] != self.my_node:
-                self.storage[update[0]] += update[4]
-                self.nodes_imgs[update[0]].append(update[5])
-            if update[1] != self.my_node:
-                self.storage[update[1]] += update[4]
-                self.nodes_imgs[update[1]].append(update[5])
+            if self.all_nodes(update[0]) is not None:
+                if update[0] != self.my_node:
+                    self.storage[update[0]] += update[4]
+                    self.nodes_imgs[update[0]].append(update[5])
+            if self.all_nodes(update[1]) is not None:
+                if update[1] != self.my_node:
+                    self.storage[update[1]] += update[4]
+                    self.nodes_imgs[update[1]].append(update[5])
             self.general_map[update[5]] = update[0:5]
 
         elif self.is_better(update[2], self.general_map[update[5]][2], update[3], self.general_map[update[5]][3], update[4], self.general_map[update[5]][4]):
             print("Update with better image: ", update[5])
             val= self.general_map[update[5]]
-            if val[0] != self.my_node:
-                self.storage[val[0]] -= val[4]
-                self.nodes_imgs[val[0]].remove(update[5])
-            if val[1] != self.my_node:
-                self.storage[val[1]] -= val[4]
-                self.nodes_imgs[val[1]].remove(update[5])
-            if update[0] != self.my_node:
-                self.storage[update[0]] += update[4]
-                self.nodes_imgs[update[0]].append(update[5])
-            if update[1] != self.my_node:
-                self.storage[update[1]] += update[4]
-                self.nodes_imgs[update[1]].append(update[5])
+            if self.all_nodes(val[0]) is not None:
+                if val[0] != self.my_node:
+                    self.storage[val[0]] -= val[4]
+                    self.nodes_imgs[val[0]].remove(update[5])
+            if self.all_nodes(val[1]) is not None:
+                if val[1] != self.my_node:
+                    self.storage[val[1]] -= val[4]
+                    self.nodes_imgs[val[1]].remove(update[5])
+            if self.all_nodes(update[0]) is not None:
+                if update[0] != self.my_node:
+                    self.storage[update[0]] += update[4]
+                    self.nodes_imgs[update[0]].append(update[5])
+            if self.all_nodes(update[1]) is not None:
+                if update[1] != self.my_node:
+                    self.storage[update[1]] += update[4]
+                    self.nodes_imgs[update[1]].append(update[5])
             self.general_map[update[5]] = update[0:5]
 
         else:
@@ -210,19 +217,17 @@ class Functions:
         """
         if self.general_map.get(hashkey) is not None:
             val = self.general_map[hashkey]
-            if val[0] != self.my_node:
-                self.storage[val[0]] -= val[4]
-                self.nodes_imgs[val[0]].remove(hashkey)
-            if val[1] != self.my_node:
-                self.storage[val[1]] -= val[4]
-                self.nodes_imgs[val[1]].remove(hashkey)
+            if self.all_nodes(val[0]) is not None:
+                if  val[0] != self.my_node:
+                    self.storage[val[0]] -= val[4]
+                    self.nodes_imgs[val[0]].remove(hashkey)
+            if self.all_nodes(val[0]) is not None:
+                if val[1] != self.my_node:
+                    self.storage[val[1]] -= val[4]
+                    self.nodes_imgs[val[1]].remove(hashkey)
 
         backup_node= min(self.storage, key=self.storage.get)
 
-        #TODO send image to backup_node
-    
-        print("Image sent to backup node: ", backup_node)
-     
         self.storage[backup_node] += self.img_map[hashkey][2]
         self.nodes_imgs[backup_node].append(hashkey)
 
@@ -238,6 +243,27 @@ class Functions:
         
         #TODO send update to all nodes
 
+    
+    def send_img(self, hashkey, node):
+        """
+        Send image to all nodes.
+        """
+        img_path = os.path.join(self.folder_path, hashkey)
+        # img = Image.open(img_path)
+        #TODO send image to node
+    
+        print("Image sent to: ", node)
+
+
+    def backup_and_update(self, hashkey):
+        """
+        Backup image to best node and send update.
+        """
+        backup_node = self.backup_node(hashkey)
+        self.general_map[hashkey] = [self.my_node, backup_node, self.img_map[hashkey][0], self.img_map[hashkey][1], self.img_map[hashkey][2]]
+        self.send_update(hashkey)
+        self.send_img(hashkey, backup_node)
+
 
     def merge_my_img(self):
         """
@@ -251,15 +277,12 @@ class Functions:
 
             if self.general_map.get(hashkey) is None:
                 print("New image:", hashkey)
-                backup_node = self.backup_node(hashkey)
-                self.general_map[hashkey] = (self.my_node, backup_node, self.img_map[hashkey][0], self.img_map[hashkey][1], self.img_map[hashkey][2])
-                self.send_update(hashkey)
+                self.backup_and_update(hashkey)
+                
                 
             elif self.is_better(self.img_map[hashkey][0], self.general_map[hashkey][2], self.img_map[hashkey][1], self.general_map[hashkey][3], self.img_map[hashkey][2], self.general_map[hashkey][4]):
                 print("Better image found: ", hashkey)
-                backup_node = self.backup_node(hashkey)
-                self.general_map[hashkey] = (self.my_node, backup_node, self.img_map[hashkey][0], self.img_map[hashkey][1], self.img_map[hashkey][2])
-                self.send_update(hashkey)
+                self.backup_and_update(hashkey)
                 
             else:
                 print("Worse image found: ", hashkey)
@@ -275,5 +298,14 @@ class Functions:
 
         self.all_nodes.remove(node)
         self.all_socks.pop(node)
+        self.storage.pop(node)     
 
-        # storage and nodes_img updates at last in case the node survives
+        for hashkey in self.nodes_imgs[node]:
+
+            # TODO UPDATE DATA EVERYTIME WE GET AN UPDATE
+
+            if self.img_map.get(hashkey) is not None:
+                print("Image: ", hashkey, " was lost")
+                self.backup_and_update(hashkey)
+
+        self.nodes_imgs.pop(node)
